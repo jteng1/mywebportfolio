5561b10c8eac977280f0c1afd60b533d1d20fa04
Justin Teng <nekasrofd@justins-mbp.lan>
2019-04-22 23:44:31 -0700

    fixed linking within website

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Web Portfolio created with React
 
 This is my personal web portfolio that was bootstrapped with create-react-app. It is created from scratch with React and React Bootstrap.
+
+## Todo
+
+Refactor code into more components<br/>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,13 +17,13 @@ export default class Skills extends Component {
                             <h3>Programming Languages</h3>
                             <Row>
                                 <p className="info">
-                                Work primarily with JavaScript and Python, with frameworks such as Express.js, React.js, Django and Flask. Primarily building projects with the MERN stack and implementing object-oriented design. Ability to learn new languages and frameworks. 
+                                Work primarily with JavaScript and Python with experience working with frameworks such as Express.js, React.js, Django and Flask. Primarily building projects with the MERN stack and implementing object-oriented design. Ability to learn new languages and frameworks as necessary. 
                                 </p>
                             </Row>
                             <h3>Database Systems</h3>
                             <Row>
                                 <p className="info">
-                                Experience working with both SQL and NoSQL databases. Mainly utilizing MongoDB in current projects.
+                                Experience working with both SQL and NoSQL databases. Mainly utilizing MongoDB in current projects but also have experience in using MySQL and PostgreSQL.
                                 </p>
                             </Row>
                         </Row>
